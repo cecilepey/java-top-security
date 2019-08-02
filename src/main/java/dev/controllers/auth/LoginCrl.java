ce474ac72438ca591ac516225591452ff11aee12
Cecile Peyras <peyrascecile@gmail.com>
2019-08-02 15:08:55 +0200

    ajout des cookies

--- src/main/java/dev/controllers/auth/LoginCrl.java
+++ src/main/java/dev/controllers/auth/LoginCrl.java
@@ -1,41 +1,61 @@
 package dev.controllers.auth;
 
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.digest.HmacAlgorithms;
+import org.apache.commons.codec.digest.HmacUtils;
+
 import dev.domains.User;
 import dev.services.LoginService;
 import dev.services.ServicesFactory;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Optional;
-
 @WebServlet("/login")
 public class LoginCrl extends HttpServlet {
 
-    private LoginService loginService = ServicesFactory.LOGIN_SERVICE;
+	private LoginService loginService = ServicesFactory.LOGIN_SERVICE;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp").forward(req, resp);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp").forward(req, resp);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String login = req.getParameter("login");
-        String password = req.getParameter("pass");
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String login = req.getParameter("login");
+		String password = req.getParameter("pass");
 
-        Optional<User> userOpt = loginService.connect(login, password);
+		Optional<User> userOpt = loginService.connect(login, password);
 
-        if (userOpt.isPresent()) {
-            User user = userOpt.get();
-            req.getSession().setAttribute("connectedUser", user);
-            resp.sendRedirect("/users/list");
-        } else {
-            req.setAttribute("errors", "les informations fournies sont incorrectes");
-            req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp").forward(req, resp);
-        }
-    }
+		if (userOpt.isPresent()) {
+			User user = userOpt.get();
+			req.getSession().setAttribute("connectedUser", user);
+
+			String key = "message top secret";
+
+			String data = user.getFirstname() + ", " + user.getLastname() + ", " + user.getLogin() + ", "
+					+ user.getAdmin().toString();
+
+			String signature = new HmacUtils(HmacAlgorithms.HMAC_SHA_256, key).hmacHex(data);
+
+			String cookieValue = Base64.getUrlEncoder().encodeToString(data.getBytes()) + "." + signature;
+
+			Cookie monCookie = new Cookie("Cookie", cookieValue);
+			monCookie.setHttpOnly(true);
+			resp.addCookie(monCookie);
+
+			resp.sendRedirect(req.getContextPath() + "/users/list");
+		} else {
+			req.setAttribute("errors", "les informations fournies sont incorrectes");
+			req.getRequestDispatcher("/WEB-INF/views/auth/login.jsp").forward(req, resp);
+		}
+	}
 }
--- src/main/java/dev/services/LoginService.java
+++ src/main/java/dev/services/LoginService.java
@@ -1,24 +1,30 @@
 package dev.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+
 import dev.domains.User;
 import dev.exceptions.AppException;
 import dev.utils.DbUtils;
 
-import java.util.List;
-import java.util.Optional;
-
-
 public class LoginService {
 
-    public Optional<User> connect(String login, String password) {
-        List<User> results = new DbUtils().executeSelect(String.format("select * from user where login='%s' and password='%s'", login, password), resultSet -> new DbUtils().resultSetToUser(resultSet));
+	public Optional<User> connect(String login, String password) {
+		List<User> results = new DbUtils().executeSelect(String.format("select * from user where login='%s'", login),
+				resultSet -> new DbUtils().resultSetToUser(resultSet));
 
-        if(results.size() > 1) {
-            throw new AppException("at least 2 users with same login");
-        }
+		if (results.size() > 1) {
+			throw new AppException("at least 2 users with same login");
+		} else if (results.size() == 1) {
+			for (User list : results) {
+				BCrypt.checkpw(password, list.getPassword());
+			}
 
-        return results.stream().findAny();
-    }
+		}
 
+		return results.stream().findAny();
+	}
 
 }
